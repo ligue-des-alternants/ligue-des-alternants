1767a3663dab8cd44819622a32265ea6666f67fa
Thomas ROBERT <thomas_3004@hotmail.fr>
2025-10-21 13:42:29 +0200

    feat: add rights & responsibilities page and update navigation

--- src/components/react/navbar/data.ts
+++ src/components/react/navbar/data.ts
@@ -1,4 +1,5 @@
 export const navigation = [
   { name: 'Accueil', href: '/' },
   { name: 'À propos', href: '/a-propos' },
+  { name: 'Nos droits et devoirs', href: '/nos-droits-et-devoirs-en-alternance' },
 ];
