e382feb0242a7ffb0be7f10028645971dbef4a65
Thomas ROBERT <thomas_3004@hotmail.fr>
2025-10-22 14:24:52 +0200

    feat: add 'Rédiger son CV' page

--- src/components/react/navbar/data.ts
+++ src/components/react/navbar/data.ts
@@ -1,5 +1,6 @@
 export const navigation = [
   { name: 'Accueil', href: '/' },
   { name: 'À propos', href: '/a-propos' },
+  { name: 'Rédiger son CV', href: '/comment-rediger-son-cv' },
   { name: 'Nos droits et devoirs', href: '/nos-droits-et-devoirs-en-alternance' },
 ];
