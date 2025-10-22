6aa28c3ba2baf1db06c5a500b4c54adfe5881437
Thomas ROBERT <thomas_3004@hotmail.fr>
2025-10-22 18:08:38 +0200

    feat: add 'Taxe d\'apprentissage' page to navigation and create corresponding content

--- src/components/react/navbar/data.ts
+++ src/components/react/navbar/data.ts
@@ -3,4 +3,5 @@ export const navigation = [
   { name: 'À propos', href: '/a-propos' },
   { name: 'Rédiger son CV', href: '/comment-rediger-son-cv' },
   { name: 'Nos droits et devoirs', href: '/nos-droits-et-devoirs-en-alternance' },
+  { name: "Taxe d'apprentissage", href: '/taxe-apprentissage' },
 ];
